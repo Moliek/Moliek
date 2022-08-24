aff9b33fa2cc82405b2e02dec874ffce2eec3e03
Moliek <90347638+Moliek@users.noreply.github.com>
2022-08-24 11:49:11 -0400

    V.03
    Added favorite candy

--- wilson_moliek_hello_world.py
+++ wilson_moliek_hello_world.py
@@ -1,4 +1,4 @@
-# Hello, World! Moliek Wilson, 08/22/22 12:35pm v0.2
+# Hello, World! Moliek Wilson, 08/24/22 11:48 v0.3
 print("Hello, world!") # comments dont have to start on the beginning of the line. 
 print("My name is moliek and I am in period 7B.")
-
+print("I like skittles.")
