f43a427f947087b3993e1f36ce349f3306789dbe
Moliek <90347638+Moliek@users.noreply.github.com>
2022-08-24 11:42:19 -0400

    Version 0.02
    Added name and period

---
+++ wilson_moliek_hello_world.py
@@ -0,0 +1,4 @@
+# Hello, World! Moliek Wilson, 08/22/22 12:35pm v0.2
+print("Hello, world!") # comments dont have to start on the beginning of the line. 
+print("My name is moliek and I am in period 7B.")
+
