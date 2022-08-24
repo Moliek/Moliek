155d1f2d08e333206ad3c0dbf4b3deb38c596564
Moliek <90347638+Moliek@users.noreply.github.com>
2022-08-24 12:01:30 -0400

    V.02
    Added age

--- wilson_moliek_hello_world.py
+++ wilson_moliek_hello_world.py
@@ -1,4 +1,9 @@
-# Hello, World! Moliek Wilson, 08/24/22 11:48 v0.3
+# Hello, World! Moliek Wilson, 08/24/22 11:48 v0.4
 print("Hello, world!") # comments dont have to start on the beginning of the line. 
 print("My name is moliek and I am in period 7B.")
 print("I like skittles.")
+my_age = 17
+print(my_age)
+print(my_age + 10)
+
+
